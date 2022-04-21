886ff3d712c3b013cc69cfed87e26ac09ef0467b
Sollarix <ramilk040@gmail.com>
2022-04-21 17:59:11 +0500

    Tutorial 12 and 13 completed!

--- 2ndLabCG.cpp
+++ 2ndLabCG.cpp
@@ -3,20 +3,34 @@
 #include<GL/freeglut.h>
 #include<glm/vec3.hpp>
 #include<glm/mat4x4.hpp>
+#include<glm/gtx/transform.hpp>
+
+#define ToRadian(x) ((x) * 3.14159265359 / 180.0f)
 
 GLuint VBO;
+GLuint IBO;
+
+unsigned int Indices[] = { 0, 3, 1,
+                           1, 3, 2,
+                           2, 3, 0,
+                           0, 2, 1 };
+
 float scale = 0.000f;
+const float ar = 1024 / 768;
+const float tanHalfFOV = tanf(ToRadian(30.0f / 2.0f));
+const float zNear = 1.0f;
+const float zFar = 1000.0f;
+const float zRange = zNear - zFar;
 
 void RenderSceneCB()
 {
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
-    scale += 0.0025f;
+    scale += 0.0500f;
 
     glEnableVertexAttribArray(0);
-    glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-
+    glColor4f(1.0f, 0.0f, 0.0f, 0.0f);
+    glDrawElements(GL_LINE_LOOP, 12, GL_UNSIGNED_INT, 0);
     //Triangle move
     glm::mat4 myMatrixMove(1.0f, 0.0f, 0.0f, 0.0f,
         0.0f, 1.0f, 0.0f, 0.0f,
@@ -36,7 +50,14 @@ void RenderSceneCB()
         0.0f, sinf(scale), 0.0f, 0.0f,
         0.0f, 0.0f, sinf(scale), 0.0f,
         0.0f, 0.0f, 0.0f, 1.0f);
-    glm::mat4 myMatrixTransformation = myMatrixMove * myMatrixRotateZ * myMatrixRotateY * myMatrixResize;
+    glm::mat4 myMatrixPersProj(1.0f / (tanHalfFOV * ar), 0.0f, 0.0f, 0.0f,
+                               0.0f, 1.0f / tanHalfFOV, 0.0f, 0.0f,
+                               0.0f, 0.0f, (-zNear - zFar) / zRange, 2.0f * zFar * zNear / zRange,
+                               0.0f, 0.0f, 1.0f, 0.0f);
+    glm::mat4 ViewMatrix = glm::lookAt(glm::vec3(0.0, 0.0, -2.0),
+                                       glm::vec3(0.0, 0.0, 0.0),
+                                       glm::vec3(1.0, 2.0, 1.0));
+    glm::mat4 myMatrixTransformation = myMatrixPersProj * ViewMatrix * myMatrixRotateY;
     glLoadMatrixf(reinterpret_cast<const float*>(&myMatrixTransformation));
 
     glutSwapBuffers();
@@ -48,7 +69,7 @@ int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowSize(1024, 768);
+    glutInitWindowSize(1280, 768);
     glutInitWindowPosition(200, 50);
     glutCreateWindow("Lab Work 2");
     GLenum res = glewInit();
@@ -58,7 +79,16 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    glm::vec3 Vertices[3] = { {0.25f, 0.25f, 0.0f}, {-0.25f, 0.25f, 0.0f}, {0.0f, -0.25f, 0.0f} };
+    glGenBuffers(1, &IBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+
+    glm::vec3 Vertices[4];
+    Vertices[0] = glm::vec3(-0.5f, -0.5f, 0.0f);
+    Vertices[1] = glm::vec3(0.0f, -0.5f, 0.5f);
+    Vertices[2] = glm::vec3(0.5f, -0.5f, 0.0f);
+    Vertices[3] = glm::vec3(0.0f, 0.5f, 0.0f);
 
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
